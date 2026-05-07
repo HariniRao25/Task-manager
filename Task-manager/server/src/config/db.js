272fdcd6e69ae7f613cdb9b2a590e3cf3d2d182f
HariniRao25 <harinijakinapelly1223@gmail.com>
2026-05-07 11:47:00 +0530

    fixed db

--- Task-manager/server/src/config/db.js
+++ Task-manager/server/src/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  const uri = process.env.MONGO_URI || 'mongodb://localhost:27017/sa';
+  const uri = process.env.MONGO_URI;
   if (!uri) throw new Error('MONGO_URI not set');
   await mongoose.connect(uri, {
     useNewUrlParser: true,
